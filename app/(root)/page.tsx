3770dc43f96be6b8d6e351dab68a1be0fb6047e3
Milk <milk@Milks-MacBook-Pro.local>
2025-01-26 15:52:23 +0100

    Root layout & contants

---
+++ app/(root)/page.tsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "Home",
+};
+
+const Homepage = () => {
+  return <div>Dive Center Manager</div>;
+};
+export default Homepage;
---
+++ lib/constants/index.ts
@@ -0,0 +1,9 @@
+export const APP_NAME =
+  process.env.NEXT_PUBLIC_APP_NAME || "Dive Center Manager";
+
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  "App to fully manage a dive center, built with NextJS, based on Traversy's course";
+
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
