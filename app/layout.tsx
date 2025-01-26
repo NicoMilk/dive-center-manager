2931f3db50d9d91bc450b07f67ec1f64a7cc0c91
NicoMilk <61079350+NicoMilk@users.noreply.github.com>
2025-01-26 15:52:23 +0100

    Root layout & contants

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/assets/styles/globals.css";
+import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from "@/lib/constants";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Dive Center Manager",
-  description:
-    "App to manage a dive center, built with NextJS, based on Traversy's course",
+  title: { template: `%s | Dive Center Manager`, default: APP_NAME },
+  description: APP_DESCRIPTION,
+  metadataBase: new URL(SERVER_URL),
 };
 
 export default function RootLayout({
