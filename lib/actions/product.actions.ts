a7a9a244864180a64855b9bec56afc6a851e9fc7
NicoMilk <61079350+NicoMilk@users.noreply.github.com>
2025-02-02 21:30:57 +0100

    Serverless environment config

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,12 +1,10 @@
 "use server";
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "@/db/prisma";
 import { convertToPlainObject } from "@/lib/utils";
 import { LATEST_PRODUCTS_LIMIT } from "../constants";
 
 // Get latest products
 export const getLatestProducts = async () => {
-  const prisma = new PrismaClient();
-
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: { createdAt: "desc" },
